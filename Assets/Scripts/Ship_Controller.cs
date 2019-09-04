dbe236317abb75645833d63329217b5cdb3e99c3
ClandestineBoy <rmh472@nyu.edu>
2019-09-03 20:44:59 -0400

    Unity Init
    Making the repo

---
+++ Assets/Scripts/Ship_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Controller : MonoBehaviour
+{
+    public KeyCode up, down;
+    public float spd;
+    public Rigidbody2D rb;
+
+    private Vaector2 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    void Update()
+    {
+        rb.velocity = Vector2.zero;
+        if (Input.GetKey(up))
+        {
+            rb.velocity = new Vector2(0,spd);
+        }
+        if (Input.GetKey(down))
+        {
+            rb.velocity = new Vector2(0, -spd);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = startPos;
+    }
+}
